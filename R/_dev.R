fe9aaf6f831e66fe9746ba2451cc2476d6c4e1d8
Daniel Vartanian <danvartan@gmail.com>
2023-11-07 00:55:55 -0300

    Batch changes

--- R/_dev.R
+++ R/_dev.R
@@ -43,24 +43,34 @@
 # quarto render --profile html
 # quarto publish gh-pages
 
+# # LaTeX
+#
+# \BeforeBeginEnvironment{}{} # {etoolbox}
+# \AtBeginEnvironment{}{}
+# \AtEndEnvironment{}{}
+# \AfterEndEnvironment{}{}
+
 # TODO
 #
 # * Fix spacing (\baselineskip).
 # * Fix SCSS. Mobile menu is appearing with white or transparent color.
+# * Fix caption sizes and spacing.
 
+# * Fix spacing in `figure` environment.
 # * Fix quinary section after skip.
 # * Find why the title of LoF, LoT, ToC and Abstract do not align with others.
 # * Fix title page spacing.
 # * Add command tag sub function.
-# * Add figure using the ABNT format (see 'formating-options.qmd').
-# * Add table using the ABNT format (see 'formating-options.qmd').
-# * Fix References section bookmark (see R package {xmpdf}).
-# * Fix '\tightlist' settings.
-# * Fix indentation of unordered and ordered lists.
 # * Fix title and table captions making the caption name (e.g., "Figure -")
 #   become tabular.
+# * Fix References section bookmark (see R package {xmpdf}).
+# * Fix spacing in `theorem` environment.
+# * Fix spacing
+# * Include `\ABNTEXchapterupperifneeded{}` in the References section config.
+# * Fix '\tightlist' settings.
+# * Fix indentation of unordered and ordered lists.
 # * Change table font sizes to be accordingly with figure font sizes.
-# * Add code post-chapters and pre-appendices automatically.
+# * Add code post-chapters, pre-appendices and pre-annexes automatically.
 # * Add numbers and letters before chapter's and appendices bookmarks.
 # * Refactor code for creating ambient variables in
 #   'quarto-pre-render-common.R' . Transform code into function and then add
@@ -72,5 +82,4 @@
 # * Create a hex logo and review badges.
 # * Add <https://fair-software.eu/>.
 # * Add <https://www.bestpractices.dev/en/projects/6244 badge>.
-# * Find a way to create double captions (top and bottom).
 # * Fix code background on dark theme.
